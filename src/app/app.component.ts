19b11405597041a2680729977e3b3c741630c9ce
tchfer <tchfernando@gmail.com>
2023-07-02 21:26:15 -0300

    Add property binding from parent to child comp.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Item } from './interfaces/iItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
+  listaDeCompra!: Array<Item>
 
-  constructor() { }
+  constructor(
+    private listaService: ListaDeCompraService
+  ) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+  }
+
+
 }
