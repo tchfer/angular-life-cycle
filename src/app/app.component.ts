545f2f8c4f4eec89be9727e2d71760f03c4ba9cc
tchfer <tchfernando@gmail.com>
2023-07-02 23:23:59 -0300

    Implement DoCheck lifehoook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 import { ListaDeCompraService } from './service/lista-de-compra.service';
 import { Item } from './interfaces/iItem';
@@ -8,7 +8,7 @@ import { Item } from './interfaces/iItem';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
   title = 'app-lista-de-compras';
   listaDeCompra!: Array<Item>;
   itemParaSerEditado!: Item;
@@ -25,4 +25,7 @@ export class AppComponent implements OnInit {
     this.itemParaSerEditado = itemSelecionado;
   }
 
+  ngDoCheck(): void {
+    this.listaService.atualizarLocalStorage();
+  }
 }
